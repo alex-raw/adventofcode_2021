593f1fc2f9a80b9ab7d22a1410a75de405e9bfa5
alex-raw <alexanderrauhutjs@gmail.com>
2021-12-01 22:35:17 +0100

    finished day 1

--- aoc_1.R
+++ aoc_1.R
@@ -1,11 +1,10 @@
 count_increases <- function(x, n) {
-  ln <- length(x)
-  x <- matrix(x, ln + 1, n)[1:ln, ] |>
-      as.matrix() |> rowSums() |> diff()
-  sum(x > 0)
+  ln <- length(x) + 1
+  x <- rowSums(matrix(x, ln, n)[-ln, ] |>
+    suppressWarnings() |> as.matrix())
+  sum(diff(x) > 0)
 }
 
 d <- as.integer(readLines("data/aoc_1"))
 count_increases(d, 1)
 count_increases(d, 3)
-
