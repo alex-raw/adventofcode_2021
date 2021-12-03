76383fbbef7a9a58be0bcb005f07f1207bdce6f7
alex-raw <alexanderrauhutjs@gmail.com>
2021-12-03 19:58:28 +0100

    cleaned R

--- aoc_3.R
+++ aoc_3.R
@@ -8,7 +8,7 @@ parse_string <- function(x) {
 vec2dec <- function(x)
   as.integer(x) |>
   paste(collapse = "") |>
-  strtoi(2)
+  strtoi(base = 2)
 
 solve_1 <- function(x) {
   counts <- colSums(x)
@@ -16,24 +16,24 @@ solve_1 <- function(x) {
   vec2dec(counts >= half) * vec2dec(counts < half)
 }
 
-get_ogr <- function(x, scrub = FALSE) {
+find_hidden <- function(x, scrub = FALSE) {
   for (i in seq_len(ncol(x))) {
-    current <- x[, i]
-    b <- sum(current) >= (nrow(x) / 2)
-    if (scrub) b <- !b
-    x <- if (b) x[current, ] else x[!current, ]
-    if (!is.matrix(x)) break
+    ids <- x[, i]
+    is_greater <- sum(ids) >= (nrow(x) / 2)
+    flip <- xor(scrub, is_greater) # additional flip for <=
+    x <- x[xor(flip, ids), ]       # flip if is greater
+    if (!is.matrix(x)) break       # have vector when one is left
   }
   x
 }
 
 solve_2 <- function(x) {
-  ogr <- get_ogr(x) |> vec2dec()
-  co2 <- get_ogr(x, scrub = TRUE) |> vec2dec()
+  ogr <- find_hidden(x) |> vec2dec()
+  co2 <- find_hidden(x, scrub = TRUE) |> vec2dec()
   ogr * co2
 }
 
-x <- parse_string(c("00100", "11110", "10110", "10111", "10101", "01111", "00111", "11100", "10000", "11001", "00010", "01010"))
+# x <- parse_string(c("00100", "11110", "10110", "10111", "10101", "01111", "00111", "11100", "10000", "11001", "00010", "01010"))
 x <- parse_string(readLines("data/aoc_3"))
 solve_1(x)
 solve_2(x)
--- aoc_3.jl
+++ aoc_3.jl
@@ -1,5 +1,28 @@
+using DelimitedFiles
+readdlm("data/aoc_3", "")
+
 data = parse.(Int, readlines("data/aoc_3"); base = 2)
 
+open("data/aoc_3") do file
+    for line in eachline(file)
+        println(line)
+    end
+end
+
+
+lol = [parse.(Bool, split(row, "")) for row in readlines("data/aoc_3")]
+
+       hcat(lol)
+
+chomp
+
 data = readlines("data/aoc_3")
+data = map(x -> parse.(Bool, split(x, "")), data)
+data = reduce(hcat, data)
+data'
+
+first(data, 1)
+
 data[1][1]
 split.(data, "")
+extract
