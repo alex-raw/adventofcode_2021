92b4c9e63bd6a8e558b36b4c49be3de8a1dd7f1e
alex-raw <alexanderrauhutjs@gmail.com>
2021-12-04 20:50:13 +0100

    R 4 finished

--- aoc_4.R
+++ aoc_4.R
@@ -6,18 +6,23 @@ parse_bingo <- function(path) list(
 check_bingo <- function(x, l)
   any(rowSums(x) == l) | any(colSums(x) == l)
 
-play <- function(nums, cards, l) {
+play <- function(nums, cards, l, part1 = TRUE) {
   dims <- c(l, l, length(cards) / l^2)
   checks <- array(FALSE, dims)
+  bingo <- logical()
 
   for (num in nums) {
     checks[cards == num] <- TRUE
+    prev_bingo <- bingo # part2
     bingo <- apply(checks, 3, check_bingo, l)
-    if (any(bingo)) break
+    if (part1 & any(bingo)) break
+    if (!part1 & all(bingo)) break
   }
 
+  if (!part1) bingo <- !prev_bingo # part2
   dim(cards) <- dims
   sum(cards[, , bingo][!checks[, , bingo]]) * num
 }
 
 with(parse_bingo("data/aoc_4"), play(nums, cards, 5))
+with(parse_bingo("data/aoc_4"), play(nums, cards, 5, part1 = FALSE))
