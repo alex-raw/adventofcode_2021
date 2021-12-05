3d939e8eb5e3c7c5b367ba090fc0f651ada75cdc
alex-raw <alexanderrauhutjs@gmail.com>
2021-12-05 23:57:14 +0100

    R 5 cleaned more

--- aoc_5.R
+++ aoc_5.R
@@ -1,8 +1,3 @@
-read_coord <- function(path) {
-  x <- gsub(",| -> ", " ", readLines(path))
-  read.table(text = x, col.names = c("x1", "y1", "x2", "y2"))
-}
-
 count_overlaps <- function(x1, y1, x2, y2) {
   x <- Map(seq, x1, x2)
   y <- Map(seq, y1, y2)
@@ -11,13 +6,11 @@ count_overlaps <- function(x1, y1, x2, y2) {
 }
 
 solve <- function(path, part1 = TRUE) {
-  d <- read_coord(path)
-  if (part1) {
-    straight <- with(d, x1 == x2 | y1 == y2)
-    d <- d[straight, ]
-  }
+  d <- gsub(",| -> ", " ", readLines(path))
+  d <- read.table(text = d, col.names = c("x1", "y1", "x2", "y2"))
+  if (part1) d <- subset(d, x1 == x2 | y1 == y2)
   with(d, count_overlaps(x1, y1, x2, y2))
 }
 
-c(solve("data/aoc_5"),
-  solve("data/aoc_5", part1 = FALSE))
+c(part1 = solve("data/aoc_5"),
+  part2 = solve("data/aoc_5", part1 = FALSE))
