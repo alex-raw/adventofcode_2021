f08c0c97500c653912ac93acae1cc9f7b1e634b6
alex-raw <alexanderrauhutjs@gmail.com>
2021-12-06 18:24:43 +0100

    Julia 6 finished

---
+++ aoc_6.jl
@@ -0,0 +1,13 @@
+function simulate_fish(fish, days)
+    counter = [count(==(n), fish) for n in 1:9]
+    for _ in 1:days
+        n = counter[1]
+        counter = circshift(counter, -1)
+        n > 0 && (counter[7] = counter[7] + n)
+    end
+    sum(counter)
+end
+
+fish = 1 .+ parse.(Int, split(readline("data/aoc_6"), ","))
+println(simulate_fish(fish, 80))
+println(simulate_fish(fish, 256))
