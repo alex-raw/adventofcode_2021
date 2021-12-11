365f80fcdb553aa518e2fa81153f81629039ecd8
alex-raw <alexanderrauhutjs@gmail.com>
2021-12-11 13:43:19 +0100

    de-spaghettified 9 R

--- aoc_9.R
+++ aoc_9.R
@@ -1,54 +1,40 @@
-parse_grid <- function(path)
-  as.matrix(read.fwf("data/aoc_9", rep(1, 100)))
+connected_components <- function(x) {
+  x <- cbind(-1L, rbind(-1L, x, -1L), -1L) # pad to avoid out of bounds
+  old_x <- label <- 0L
+  repeat { # repeat because too dumb for part 2 of the algo
+    old <- x
+    for (j in seq_len(ncol(x)))            # raster
+      for (i in seq_len(nrow(x)))          # scanning
+        if (x[i, j] != -1L) {              # if it is a basin
+          label <- label + 1L              # create new label
+          nb <- c(                         # get neighboring elements
+              x[i - 1L, j], x[i + 1L, j],
+              x[i, j - 1L], x[i, j + 1L])
+          nb <- nb[nb > 0L]
+          x[i, j] <- if (length(nb) == 0L) # if no neighbor
+            label else min(nb)             # assign new else smallest label
+        } # step 2 (missing): remember and link label associations
+    if (identical(x, old))
+      return(x)
+  }
+}
 
-find_minim <- function(m)                # pad 9 to catch minima at the bound
+find_minim <- function(m) # pad 9 to catch minima at the bound
   diff(sign(diff(c(9, m, 9))))
 
-connected_components <- function(x) {
-  old_x <- label <- -1L
-  while (!identical(x, old_x)) {         # repeat because too dumb for part 2 of the algo
-    old_x <- x
-    for (j in seq_len(ncol(x))) {        # raster
-      for (i in seq_len(nrow(x))) {      # scanning
-        if (x[i, j]) {                   # if it is a basin
-          neighbors <- c(                # get neighboring elements
-            x[i - 1L, j], x[i + 1L, j],
-            x[i, j - 1L], x[i, j + 1L])
-          neighbors <- neighbors[neighbors < 0L]
-
-          if (length(neighbors) == 0L) { # if no neighbor, uniquely label
-            x[i, j] <- label
-          } else {
-            x[i, j] <- max(neighbors)    # assign smallest label
-          }
-          label <- label - 1L            # create label, + -> unmarked, - -> marked
-        }
-      }
-    }
-  }
-  x
-}
-
-solve <- function(x, part1 = TRUE) {
+solve1 <- function(x) {
   rows <- t(apply(x, 1L, find_minim))
   cols <- apply(x, 2L, find_minim)
-
-  if (part1) return(
-    sum(x[rows == 2L & cols == 2L] + 1L)
-  )
-
-  rows <- rows + cols >= 0L
-  rows[x == 9L] <- -2L                  # -2s are maxima
-  x <- rows >= 0L
-  x <- cbind(0L, rbind(0L, x, 0L), 0L)  # pad to avoid out of bounds
-
-  ans <- connected_components(x)
-  ans <- table(ans)
-  ans[names(ans) != 0] |>
-  sort() |> tail(3) |> prod()
+  sum(x[rows == 2L & cols == 2L] + 1L)
 }
 
-x <- parse_grid("data/aoc_9")
-c(part1 = solve(x),
-  part2 = solve(x, part1 = FALSE))
+solve2 <- function(x) {
+  y <- array(0L, dim(x))
+  y[x == 9L] <- -1L  # mark basin margins
+  tabulate(connected_components(y))[-1L] |> # take away counts of zeros
+  sort() |> tail(3L) |> prod()
+}
 
+x <- as.matrix(read.fwf("data/aoc_9", rep(1, 100)))
+c(part1 = solve1(x),
+  part2 = solve2(x)) #|> system.time()
